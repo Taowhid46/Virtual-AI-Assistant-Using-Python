53ef1215f087c4a7108cb3294d1990bc581705cc
Taowhid46 <81023839+Taowhid46@users.noreply.github.com>
2021-07-11 22:48:18 +0600

    3rd Commit

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
 try:
     with sr.Microphone() as source:
         print('Listening....')
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
         command = command.lower()
-        if 'what' in command:
+        if 'hi' in command:
             print(command)
+            talk(command)
 
 except:
     pass
