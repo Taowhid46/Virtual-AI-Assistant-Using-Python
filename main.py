d5caed37547ba3ad3fc33b1dc99ebc7108acf465
Taowhid46 <81023839+Taowhid46@users.noreply.github.com>
2021-07-13 23:36:51 +0600

    4th Commit

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('Listening....')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'hi' in command:
-            print(command)
-            talk(command)
 
-except:
-    pass
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening....')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'tom' in command:
+                command = command.replace('tom', '')
+                print(command)
+                talk(command)
+    except:
+        pass
+    return command
+
+
+def run_mac():
+   command = take_command()
+   print(command)
+   if 'play' in command:
+       song = command.replace('play', '')
+       talk('playing ' + song)
+       pywhatkit.playonyt(song)
+
+
+run_mac()
